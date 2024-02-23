26ecd95faa8370f05071bd2fe21f8e22a7b802a6
joshjal <joshuaalevine3@gmail.com>
2024-02-22 23:05:37 -0500

    LazyVim Config

---
+++ lua/plugins/tailwind.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {},
+      },
+    },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+  {
+    "laytan/tailwind-sorter.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    build = "cd formatter && npm i && npm run build",
+    config = true,
+  },
+}
