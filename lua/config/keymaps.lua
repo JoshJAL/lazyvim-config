23812bc8535b49897e728280d3de32a95f6a953e
joshjal <JoshuaALevine3@gmail.com>
2024-03-14 03:07:11 -0400

    update dot files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>t", require("toggleterm").toggle, { noremap = true, silent = true, desc = "ToggleTerm" })
+vim.keymap.set("n", "<leader>T", function()
+  require("toggleterm").toggle(2)
+end, { noremap = true, silent = true, desc = "ToggleTerm2" })
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,6 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  lazy = true,
+  config = true,
+}
